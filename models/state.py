0bf8e2fc388083be6746c5acb807162496ea6fc4
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-13 12:11:26 -0500

    Hbnb Clone, MySQL, change attributes to SQL and create relasionship with cities

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        'City', cascade="all,delete,delete-orphan",
+        backref=backref('state', cascade='all, delete'),
+        passive_deletes=True, single_parent=True)
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Give the City instances depending on the current State
+
+            Returns:
+                [list]: List of City instances with state_id equals to the current State.id
+            """
+            from models import storage
+            from models.city import City
+            cities_ls = []
+            for key, value in storage.all(City):
+                if value.state_id == self.id:
+                    cities_ls.append(value)
+            return cities_ls
