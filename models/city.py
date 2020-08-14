8f4db02e37e19d0cebb792722a60f7efa33e24d7
nicolasquinchia <nicolasquinchia@gmail.com>
2020-08-13 19:45:37 -0500

    Hbnb, My SQL, change relationship statements

--- models/city.py
+++ models/city.py
@@ -9,10 +9,8 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60, ForeignKey('states.id', ondelete='CASCADE')),
-                      nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship(
         'Place',
-        cascade='all',
-        backref=backref('cities', cascade='all'),
-        passive_deletes=True)
+        backref='cities',
+        cascade='all, delete-orphan')
