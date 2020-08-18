9631c7df2bd666f91d606c049eda573396d64302
Katorea132 <katorea132@gmail.com>
2020-08-17 19:14:31 -0500

    Added 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""meeeh
+"""
+
+from fabric.operations import local, run, put, env
+import os
+from datetime import datetime
+
+
+env.hosts = ['34.75.178.156', '35.231.231.185']
+env.user = 'ubuntu'
+
+def do_deploy(archive_path):
+    """
+    blahhh
+    """
+
+    if not os.path.exists(archive_path):
+        return False
+
+    name = archive_path.split('/')[-1]
+
+    untar = "/data/web_static/releases/{}".format(name.replace('.tgz', ''))
+
+    put(archive_path, '/tmp')
+    run('mkdir -p ' + untar)
+    run('tar -xzf /tmp/{} -C {}'.format(name, untar))
+    run('rm /tmp/{}'.format(name))
+    run("mv {}/web_static/* {}".format(untar, untar))
+    run("rm -rf {}/web_static".format(untar))
+    run("rm -rf /data/web_static/current")
+    run("ln -s {} /data/web_static/current".format(untar))
