aadcfb5d57aa7e6a6f322b22e1778bbe48c60b9b
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-02 22:00:33 -0500

    web_flask, starts a web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""
-Experimenting with flask
-"""
+"""Module that starts a Flask web application
+    """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def initial():
-    """Initial
+def hello_hbnb():
+    """ Method that returns Hello test for a
+    web server request
+        [str]: Display "Hello HBNB!"
     """
     return "Hello HBNB!"
 
 
-if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
