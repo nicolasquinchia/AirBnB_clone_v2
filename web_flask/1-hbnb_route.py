2f1a9e049ad2ed6101a89e6a4c9d7fcd613c2503
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-02 22:09:51 -0500

    web_flask, starts web app adds new site

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""
-Experimenting with flask
-"""
+"""Module that starts a Flask web application
+    """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def initial():
-    """Initial
+def hello_hbnb():
+    """ Method that returns Hello test for a
+    web server request
+        [str]: Display "Hello HBNB!"
     """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """hbnb
+    """ Method that returns HBNB text test for a
+    web server request
+        [str]: Display HBNB
     """
     return "HBNB"
 
-if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
