d96ca9cdfd636d2d0374a101f6078b3572a133f5
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-02 22:17:39 -0500

    web_flask, starts web app adds new site with paramther in the URL

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
-"""
-Experimenting with flask
-"""
+"""Module that starts a Flask web application
+    """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def initial():
-    """Initial
+def hello_hbnb():
+    """ Method that returns Hello test for a
+    web server request
+        [str]: Display "Hello HBNB!"
     """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """hbnb
+    """ Method that returns HBNB text test for a
+    web server request
+        [str]: Display HBNB
     """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def ctext(text):
-    """Returns the letter c plus the ext
+def display_text(text):
+    """ Method that returns "C <text>
+    Args:
+        text ([str]): URL variable
+    Returns:
+        [str]: Display "C " followed by the value of the text variable
     """
-    return "C %s" % text.replace('_', ' ')
+    return "C {}".format(text.replace('_', ' '))
 
-if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
