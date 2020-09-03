dca2ee973a55905e5277707b509b87cc96788fe0
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-02 23:09:23 -0500

    web_flask, starts web app adds new site with paramther in the URL, integer eval

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,45 +1,68 @@
 #!/usr/bin/python3
-"""
-Experimenting with flask
-"""
+"""Module that starts a Flask web application
+    """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def initial():
-    """Initial
+def hello_hbnb():
+    """ Method that returns Hello test for a
+    web server request
+        [str]: Display "Hello HBNB!"
     """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """hbnb
+    """ Method that returns HBNB text test for a
+    web server request
+        [str]: Display HBNB
     """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def ctext(text):
-    """Returns the letter c plus the ext
+def display_text_C(text):
+    """ Method that returns "C <text>
+    Args:
+        text ([str]): URL variable
+    Returns:
+        [str]: Display "C " followed by the value of the text variable
     """
-    return "C %s" % text.replace('_', ' ')
+    return "C {}".format(text.replace('_', ' '))
 
 
-@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def ptext(text):
-    """Python text
+def display_text_pythonic(text='is cool'):
+    """Method that returns "Python <text>
+
+    Args:
+        text (str, optional): text after Python. Defaults to 'is cool'.
+
+    Returns:
+        [str]: Display "Python " followed by the value of the text variable
     """
-    return "Python %s" % text.replace('_', ' ')
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """Numbers
-    """
-    return "%d is a number" % n
+def display_text_integer(n):
+    """Method that returns n is a number
+    if n is a integer
 
-if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
+    Args:
+        n ([int]): Integer to return
+
+    Returns:
+        [str]: n is a number
+    """
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
