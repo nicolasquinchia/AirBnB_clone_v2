35dd93dc0bebfa4faedebd996afb9a4b337025d6
nicolasquinchia <nicolasquinchia@gmail.com>
2020-09-02 22:49:04 -0500

    web_flask, starts web app adds new site with paramther in the URL, and a default value

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,38 +1,54 @@
 #!/usr/bin/python3
-"""
-Experimenting with flask
-"""
+"""Module that starts a Flask web application
+    """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def initial():
-    """Initial
+def hello_hbnb():
+    """ Method that returns Hello test for a
+    web server request
+        [str]: Display "Hello HBNB!"
     """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """hbnb
+    """ Method that returns HBNB text test for a
+    web server request
+        [str]: Display HBNB
     """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def ctext(text):
-    """Returns the letter c plus the ext
+def display_text_C(text):
+    """ Method that returns "C <text>
+    Args:
+        text ([str]): URL variable
+    Returns:
+        [str]: Display "C " followed by the value of the text variable
     """
-    return "C %s" % text.replace('_', ' ')
+    return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def ptext(text):
-    """Python text
-    """
-    return "Python %s" % text.replace('_', ' ')
+def display_text_pythonic(text):
+    """Method that returns "Python <text>
 
-if __name__ == '__main__':
-    app.run("0.0.0.0", 5000)
+    Args:
+        text (str, optional): text after Python. Defaults to 'is cool'.
+
+    Returns:
+        [str]: Display "Python " followed by the value of the text variable
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
